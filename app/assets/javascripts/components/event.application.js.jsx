42604e8b096f291594688349f732d3823b82396e
Padrig Tuck <Padrig@gmail.com>
2016-09-07 15:35:17 -0400

    search function added

--- app/assets/javascripts/components/event.application.js.jsx
+++ app/assets/javascripts/components/event.application.js.jsx
@@ -1,14 +1,40 @@
 var EventApplication = React.createClass({
+  getInitialState: function() {
+    return { events: [] };
+  },
+  componentDidMount: function() {
+    this.getDataFromApi();
+  },
+  getDataFromApi: function() {
+    var self = this;
+    $.ajax({
+      url: '/api/events',
+      success: function(data) {
+        self.setState({ events: data });
+      },
+      error: function(xhr, status, error) {
+        alert('Cannot get data from API: ', error);
+      }
+    });
+  },
+  handleSearch: function(events) {
+    this.setState({ events: events });
+  },
   render: function() {
     return(
       <div className="container">
         <div className="jumbotron">
           <h1>React to-do</h1>
           <p>Padrig Tuck</p>
+          <div className="row">
+            <div className="col-md-4">
+              <SearchForm handleSearch={this.handleSearch} />
+            </div>
+          </div>
         </div>
         <div className="row">
           <div className="col-md-12">
-            <EventTable />
+            <EventTable events={this.state.events} />
           </div>
         </div>
       </div>
