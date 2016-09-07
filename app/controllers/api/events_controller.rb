9f17ef61c4f3333813e9d708f229d6c895a65f0b
Padrig Tuck <Padrig@gmail.com>
2016-09-07 15:48:59 -0400

    fix

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -7,7 +7,7 @@ module Api
     def search
       query = params[:query]
       events = Event.where('name LIKE ? OR place LIKE ? OR description LIKE ?',
-                           "%#{query}", "%#{query}", "%#{query}")
+                           "%#{query}%", "%#{query}%", "%#{query}%")
       render json: events
     end
   end
